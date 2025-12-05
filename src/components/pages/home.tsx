2bd4c5a5ca4e60f2b9e46c6dc5b10187bde423c0
nainghtetlinn <naing.work.it@gmail.com>
2025-12-05 22:31:20 +0630

    feat: hero section text

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,11 +1,14 @@
 import { Navbar } from "../navbar";
-import { HeroSection } from "./sections/hero";
+import { HeroBackground, HeroSection } from "./sections/hero";
 
 export const HomePage = () => {
   return (
     <main className="bg-background relative min-h-screen w-full">
       <Navbar />
-      <HeroSection />
+      <div className="relative">
+        <HeroBackground />
+        <HeroSection />
+      </div>
       <div className="min-h-screen"></div>
       <div className="min-h-screen"></div>
     </main>

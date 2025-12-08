4df424e0065630d0fe54e00f2321adf698967a7a
nainghtetlinn <naing.work.it@gmail.com>
2025-12-08 23:32:55 +0630

    fix: warning in typewriter

--- src/components/ui/typewriter.tsx
+++ src/components/ui/typewriter.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { motion, type Variants } from "motion/react";
 
 import { cn } from "@/lib/utils";
@@ -52,7 +52,7 @@ const Typewriter = ({
   const [isDeleting, setIsDeleting] = useState(false);
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
-  const texts = Array.isArray(text) ? text : [text];
+  const texts = useMemo(() => (Array.isArray(text) ? text : [text]), [text]);
 
   useEffect(() => {
     let timeout: NodeJS.Timeout;

2328a164a904d2f542d7ab3b3566aa0261127480
nainghtetlinn <naing.work.it@gmail.com>
2025-12-09 23:39:10 +0630

    fix: remove bg two circles

--- src/components/pages/sections/hero.tsx
+++ src/components/pages/sections/hero.tsx
@@ -170,9 +170,7 @@ const HeroBackground = () => {
         style={{
           backgroundImage: `
         linear-gradient(to right, ${isLight ? "rgba(229,231,235,0.8)" : "rgba(75, 85, 99, 0.4)"} 1px, transparent 1px),
-        linear-gradient(to bottom, ${isLight ? "rgba(229,231,235,0.8)" : "rgba(75, 85, 99, 0.4)"} 1px, transparent 1px),
-        radial-gradient(circle 500px at 20% 80%, rgba(139,92,246,0.3), transparent),
-        radial-gradient(circle 500px at 80% 20%, rgba(59,130,246,0.3), transparent)
+        linear-gradient(to bottom, ${isLight ? "rgba(229,231,235,0.8)" : "rgba(75, 85, 99, 0.4)"} 1px, transparent 1px)
       `,
           backgroundSize: "48px 48px, 48px 48px, 100% 100%, 100% 100%",
         }}

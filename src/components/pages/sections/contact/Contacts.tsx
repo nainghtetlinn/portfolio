49bfb18e32a1e19ca0c8e50693ada0da64d1d45b
nainghtetlinn <naing.work.it@gmail.com>
2025-12-19 10:40:27 +0630

    fix: typo

--- src/components/pages/sections/contact/Contacts.tsx
+++ src/components/pages/sections/contact/Contacts.tsx
@@ -28,9 +28,9 @@ export const Contacts = () => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="lucide lucide-twitter-icon lucide-twitter size-5 md:size-7"
         >
           <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
@@ -44,9 +44,9 @@ export const Contacts = () => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="lucide lucide-linkedin-icon lucide-linkedin size-5 md:size-7"
         >
           <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
@@ -62,9 +62,9 @@ export const Contacts = () => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="lucide lucide-mail-icon lucide-mail size-5 md:size-7"
         >
           <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />

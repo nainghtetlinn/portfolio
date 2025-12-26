ff4ef0e7c3820db652b2ab436e441cb85b42d619
nainghtetlinn <naing.work.it@gmail.com>
2025-12-26 13:16:28 +0630

    update: project

--- src/components/pages/sections/projects/data.ts
+++ src/components/pages/sections/projects/data.ts
@@ -1,23 +1,8 @@
 export const PROJECTS = [
   {
-    name: "naingcn1",
-    description: "A Shadcn Components Library",
-    techs: ["Typescript", "NextJs", "TailwindCSS"],
-    image: "/projects/naingcn.png",
-    github: "https://github.com/nainghtetlinn/naingcn/",
-    url: "https://naingcn.vercel.app/",
-  },
-  {
-    name: "naingcn2",
-    description: "A Shadcn Components Library",
-    techs: ["Typescript", "NextJs", "TailwindCSS"],
-    image: "/projects/naingcn.png",
-    github: "https://github.com/nainghtetlinn/naingcn/",
-    url: "https://naingcn.vercel.app/",
-  },
-  {
-    name: "naingcn3",
-    description: "A Shadcn Components Library",
+    name: "naingcn",
+    description:
+      "A modular UI component library inspired by shadcn/ui, built with Next.js, Tailwind CSS, and shadcn(canery), including a documentation website with live component previews powered by Fumadocs.",
     techs: ["Typescript", "NextJs", "TailwindCSS"],
     image: "/projects/naingcn.png",
     github: "https://github.com/nainghtetlinn/naingcn/",

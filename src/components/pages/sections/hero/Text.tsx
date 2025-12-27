ff4399716e87f0a294cdf20451f263a3558d193f
nainghtetlinn <naing.work.it@gmail.com>
2025-12-27 14:10:24 +0630

    fix: update hero text

--- src/components/pages/sections/hero/Text.tsx
+++ src/components/pages/sections/hero/Text.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import env from "@/config/env";
+import { cn } from "@/lib/utils";
 import { ArrowUpRight, Download } from "lucide-react";
 import { motion } from "motion/react";
 import Link from "next/link";
@@ -10,16 +12,21 @@ export const Text = () => {
     <div className="w-full md:flex-1">
       {/* Status Badge */}
       <motion.div
-        initial={{ opacity: 0, x: -20 }}
+        initial={{ opacity: 0, x: -30 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6, delay: 0.1 }}
         className="mb-4 inline-flex items-center gap-2"
       >
         <div className="bg-background shrink-0 border px-2 pb-1">
           <span className="text-foreground/80 mr-2 font-mono text-xs">
-            Available
+            {env.NEXT_PUBLIC_AVAILABLE_STATUS ? "Available" : "Unavailable"}
           </span>
-          <span className="inline-block h-2 w-2 animate-ping rounded-full bg-green-500" />
+          <span
+            className={cn(
+              "inline-block h-2 w-2 animate-ping rounded-full",
+              env.NEXT_PUBLIC_AVAILABLE_STATUS ? "bg-green-500" : "bg-red-500",
+            )}
+          />
         </div>
         <div className="bg-secondary/60 h-px w-12" />
         <span className="text-foreground/80 shrink-0 font-mono text-xs md:text-sm">
@@ -29,8 +36,8 @@ export const Text = () => {
 
       {/* Main Heading */}
       <motion.h1
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6, delay: 0.2 }}
         className="mb-4 text-3xl leading-tight font-semibold md:text-4xl lg:text-5xl xl:text-6xl"
       >
@@ -42,8 +49,8 @@ export const Text = () => {
 
       {/* Description */}
       <motion.p
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6, delay: 0.3 }}
         className="text-foreground/80 max-w-2xl font-mono text-sm lg:text-lg"
       >
@@ -51,9 +58,9 @@ export const Text = () => {
         problems using modern technologies.
       </motion.p>
       <motion.p
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.3 }}
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
         className="text-foreground/80 max-w-2xl font-mono text-sm lg:text-lg"
       >
         My core stack includes React, Next.js, Node.js, MongoDB, PostgreSQL and
@@ -61,9 +68,9 @@ export const Text = () => {
       </motion.p>
 
       <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 0.4 }}
+        initial={{ opacity: 0, x: -30 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.6, delay: 0.5 }}
         className="mt-5 flex flex-col items-center gap-4 max-md:mx-auto max-md:justify-center sm:flex-row"
       >
         <Button asChild size="lg" className="group/btn border-2 font-medium">

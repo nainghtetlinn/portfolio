41d84f9cf0a07e4e0fcbdd311a29eeb98ef903eb
nainghtetlinn <naing.work.it@gmail.com>
2025-12-27 14:18:41 +0630

    fix: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,11 @@ import { siteConfig } from "@/config/site";
 export const Footer = () => {
   return (
     <footer className="bg-secondary text-secondary-foreground font-mono">
-      <div className="container mx-auto flex items-center justify-between px-4 py-6 md:px-8">
-        <h6>&copy; 2025 Naing Htet Linn. All rights reserved.</h6>
-        <ul className="flex items-center gap-4">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 py-6 md:flex-row md:px-8">
+        <h6 className="text-sm md:text-base">
+          &copy; 2025 Naing Htet Linn. All rights reserved.
+        </h6>
+        <ul className="flex items-center gap-4 text-sm md:text-base">
           <li className="hover:text-primary transition-all duration-300">
             <a href={siteConfig.github}>Github</a>
           </li>

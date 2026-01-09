f93884dea666a927dfcfd9ddf48da6c3cdcf5010
nainghtetlinn <naing.work.it@gmail.com>
2026-01-09 21:42:43 +0630

    chore: update contacts

--- src/components/pages/sections/contact/index.tsx
+++ src/components/pages/sections/contact/index.tsx
@@ -8,7 +8,7 @@ export const ContactSection = () => {
   return (
     <section id="contact">
       <div className="relative z-0 border-t border-b">
-        <main className="relative container mx-auto h-96 border-r border-l lg:h-140">
+        <main className="relative container mx-auto min-h-96 border-r border-l lg:min-h-120">
           <SectionLabel num="04." text="Contact" />
 
           <div className="relative space-y-6 px-4 md:px-8">
